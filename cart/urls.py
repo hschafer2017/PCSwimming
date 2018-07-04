24e3c375d4b55d6076da56dd1ceb987a14967998
Haley Schafer <hschafer@umich.edu>
2018-07-04 10:44:05 +0000

    remove from cart function

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import view_cart, add_to_cart
+from .views import view_cart, add_to_cart, remove_item
 
 urlpatterns = [
-    path('cart/', view_cart, name='view_cart'),
-    path('add/', add_to_cart, name='cart')
+    path('', view_cart, name='view_cart'),
+    path('add/', add_to_cart, name='cart'), 
+    path('remove/', remove_item, name='remove')
     ]
--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
 from products.models import Product
 
 # Create your views here - CART.
+
+
 def view_cart(request):
     cart = request.session.get('cart', {})
     
@@ -21,12 +23,23 @@ def view_cart(request):
         
     return render(request, 'cart/cart.html', {'cart': products, 'totals': totals})
 
+# Adds items to the cart and redirects to the cart after adding
 def add_to_cart(request):
     id = request.POST['product_id']
     products = get_object_or_404(Product, pk=id)
     cart = request.session.get('cart', {})
     cart[id] = cart.get(id, 0) + 1
     request.session['cart'] = cart
-    
-    # Redirecting to the cart
+    return redirect('view_cart')
+ 
+# Removes only one item from cart if quantity is more than 1.     
+def remove_item(request): 
+    id = request.POST['product_id']
+    products = get_object_or_404(Product, pk=id)
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] -= 1
+        if cart[id] == 0:
+            del cart[id]
+    request.session['cart'] = cart
     return redirect('view_cart')
