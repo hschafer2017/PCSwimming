d159adcd2bcbba803598d69bfef55c1c55fec7da
Haley Schafer <hschafer@umich.edu>
2018-07-11 10:49:38 +0000

    landing.html added and working

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def get_index(request): 
-    return render(request, 'home/index.html')
+    return render(request, 'home/landing.html')
