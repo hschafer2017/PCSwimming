5091f9e788db0579446847aa41e26db16e78b0ee
Haley Schafer <hschafer@umich.edu>
2018-08-01 22:35:00 +0000

    initial styling for product details

--- products/views.py
+++ products/views.py
@@ -8,5 +8,6 @@ def get_products(request):
     return render(request, 'products/products.html', {'products': products})
 
 def product_detail(request, pk):
+    products = Product.objects.all()
     product = get_object_or_404(Product, pk=pk)
-    return render(request, 'products/productdetail.html', {'product': product})
+    return render(request, 'products/productdetail.html', {'product': product, 'products': products})
