ce4a1b786c68e7596748e6e2ac62c968c6f45540
Haley Schafer <hschafer@umich.edu>
2018-08-25 16:35:24 +0000

    If logged in, redirect to events page on going to site

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def get_index(request): 
+    if request.user.is_authenticated: 
+        return redirect('get_events')
     return render(request, 'home/landing.html')
