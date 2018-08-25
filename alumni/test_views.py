5cc0268d110972e23ac72c7a9dac4eeedb801d49
Haley Schafer <hschafer@umich.edu>
2018-08-25 18:00:22 +0000

    blocked Swimmers from viewing alumni page with tests

--- alumni/test_views.py
+++ alumni/test_views.py
@@ -1,67 +1,80 @@
 from django.test import TestCase
 from .models import AlumPost
 from .forms import AlumPostForm
+from accounts.models import Alumni, Swimmer
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 
 class TestAlumPostViews(TestCase):
+    """Tests that you have to be registed as an Alumni to view Alumni page"""
     def test_get_alumposts_page(self):
         User.objects.create_user(
-            username='Alumniuser1', 
+            username='Alumniuser1',
             email='Alumniuser1@example.com',
             password='password1')
+        alumni = Alumni.objects.create(user_id='1', graduated='2009')
         self.client.login(username='Alumniuser1', password='password1')
         page = self.client.get("/alumni/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "alumni/alumniposts.html")
+    
+    def test_get_alumposts_page_as_swimmer(self):
+        """Tests that Swimmers are blocked from the Alumni page"""
+        User.objects.create_user(
+            username='Swimuser1',
+            email='Swimuser1@example.com',
+            password='password1')
+        swimmer = Swimmer.objects.create(user_id='1', graduation_year='2019')
+        self.client.login(username='Swimuser1', password='password1')
+        page = self.client.get("/alumni/")
+        self.assertEqual(page.status_code, 403)
     
     def test_get_new_alumpost_page(self):
         User.objects.create_user(
-            username='Alumniuser1', 
+            username='Alumniuser1',
             email='Alumniuser1@example.com',
             password='password1')
+        alumni = Alumni.objects.create(user_id='1', graduated='2009')
         self.client.login(username='Alumniuser1', password='password1')
         page = self.client.get("/alumni/new/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "alumni/alumnipostform.html")
     
     def test_create_new_alumpost_form(self):
-        
         User.objects.create_user(
             username='Alumniuser1', 
             email='Alumniuser1@example.com',
             password='password1')
+        alumni = Alumni.objects.create(user_id='1', graduated='2009')
         self.client.login(username='Alumniuser1', password='password1')
-        
-        alum_post = AlumPost.objects.create(title='Test Alumni Post', content='Test Alumni Post Content')
+
+        alum_post = AlumPost.objects.create(title='Test Alumni Post',
+                                            content='Test Alumni Post Content')
         response = self.client.get("/alumni/")
-        
+
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "alumni/alumniposts.html")
-        
+
     def test_view_alumpost_detail(self):
-        
         User.objects.create_user(
-            username='Alumniuser1', 
+            username='Alumniuser1',
             email='Alumniuser1@example.com',
             password='password1')
+        alumni = Alumni.objects.create(user_id='1', graduated='2009')
         self.client.login(username='Alumniuser1', password='password1')
-        
-        alum_post = AlumPost.objects.create(title='Test Alumni Post', content='Test Alumni Post Content')
+        alum_post = AlumPost.objects.create(title='Test Alumni Post',
+                                            content='Test Alumni Post Content')
         response = self.client.get("/alumni/1/".format(alum_post.pk))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "alumni/alumnipostdetail.html")
-    
+
     def test_get_edit_page_for_item_that_does_not_exist(self):
         page = self.client.get("alumni/1")
         self.assertEqual(page.status_code, 404)
     
     def test_get_delete_post(self): 
         User.objects.create_user(
-            username='Alumniuser1', 
+            username='Alumniuser1',
             email='Alumniuser1@example.com',
             password='password1')
+        alumni = Alumni.objects.create(user_id='1', graduated='2009')
         self.client.login(username='Alumniuser1', password='password1')
         
         alum_post = AlumPost.objects.create(title='Test Alumni Post', content='Test Alumni Post Content')
--- alumni/views.py
+++ alumni/views.py
@@ -1,43 +1,65 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 from .models import AlumPost
 from .forms import AlumPostForm
+from accounts.models import Alumni
 from django.contrib import messages
-
-# Create your views here - ALUMNI.
 
 
 @login_required(login_url='/accounts/login/')
 def get_alum_posts(request):
-    alum_posts = AlumPost.objects.all()
-    return render(request, "alumni/alumniposts.html",
+    """If the user is not an Alumni, return HttpResponseForbidden"""
+    try:
+        if (request.user.is_superuser or
+                request.user.alumni.graduated is not None):
+                alum_posts = AlumPost.objects.all()
+
+    except Alumni.DoesNotExist:
+        return HttpResponseForbidden()
+
+    return render(request, "alumni/alumniposts.html", 
                   {"alum_posts": alum_posts})
 
 
 def new_alum_post(request):
-    if request.method == "POST":
-        form = AlumPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.owner = request.user
-            post.save()
-            return redirect('get_alum_posts')
-    else:
-        form = AlumPostForm()
+    try:
+        if (request.user.is_superuser or
+            request.user.alumni.graduated is not None):
+            if request.method == "POST":
+                form = AlumPostForm(request.POST, request.FILES)
+                if form.is_valid():
+                    post = form.save(commit=False)
+                    post.owner = request.user
+                    post.save()
+                    return redirect('get_alum_posts')
+            else:
+                form = AlumPostForm()
+
+    except Alumni.DoesNotExist:
+        return HttpResponseForbidden()
 
     return render(request, 'alumni/alumnipostform.html', {'form': form})
 
 
 def alum_post_detail(request, pk):
-    alum_posts = AlumPost.objects.all()
-    post = get_object_or_404(AlumPost, pk=pk)
+    try:
+        if (request.user.is_superuser or
+            request.user.alumni.graduated is not None):
+            alum_posts = AlumPost.objects.all()
+            post = get_object_or_404(AlumPost, pk=pk)
+
+    except Alumni.DoesNotExist:
+        return HttpResponseForbidden()
+
     return render(request, "alumni/alumnipostdetail.html",
                   {'alum_posts': alum_posts, 'post': post})
 
 
 def delete_alum_post(request):
     pk = request.POST['blogs_id']
-    if request.user.is_authenticated and request.user == alumpost.owner or request.user.is_superuser:
+    if (request.user.is_authenticated and request.user == alumpost.owner or 
+        request.user.is_superuser):
         if request.method == 'POST':
             blogs = get_object_or_404(AlumPost, pk=pk)
             try:
@@ -53,7 +75,8 @@ def delete_alum_post(request):
 
 def edit_alum_post(request, pk):
     post = get_object_or_404(AlumPost, pk=pk)
-    if request.user.is_authenticated and request.user == post.owner or request.user.is_superuser:
+    if (request.user.is_authenticated and request.user == post.owner or 
+        request.user.is_superuser):
         if request.method == "POST":
             form = AlumPostForm(request.POST, request.FILES, instance=post)
             if form.is_valid():
