0df73e21eb021136854358022db9f9aa25eaeb40
Haley Schafer <hschafer@umich.edu>
2018-08-26 11:08:49 +0000

    restrictions to product access and HTML tidy up

--- products/test_views.py
+++ products/test_views.py
@@ -1,21 +1,26 @@
 from django.test import TestCase
 from .models import Product
 from django.contrib.auth.models import User
-
+from accounts.models import Swimmer, Alumni
 
 class TestProductViews(TestCase):
 
-    def test_get_product_page(self):
+    def test_get_product_page_as_swimmer(self):
+        user = User.objects.create_user(
+            username='user1',
+            email='user1@example.com',
+            password='password1')
+        swimmer = Swimmer.objects.create(user_id='1', graduation_year='2019')
+        self.client.login(username='user1', password='password1')
         page = self.client.get("/products/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "products/products.html")
 
     def test_view_product_detail(self):
         User.objects.create_user(
             username='TestSwimmer1',
             email='TestSwimmer1@example.com',
             password='password1')
-
+        swimmer = Swimmer.objects.create(user_id='1', graduation_year='2019')
         self.client.login(username='TestSwimmer1', password='password1')
         Product.objects.create(name='Test Product',
                                description='Some test content.', price='2.00',
@@ -24,4 +29,3 @@ class TestProductViews(TestCase):
         response = self.client.get("/products/1/".format(Product.pk))
 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "products/productdetail.html")
--- products/views.py
+++ products/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.http import HttpResponseForbidden
 from .models import Product
+from django.contrib.auth.models import User
+from accounts.models import Swimmer
+
+"""
+Only swimmers have access to the products/shop page. If the user is not 
+    a swimmer or a superuser, return HttpResponseForbidden. 
+"""
 
 
 def get_products(request):
-    products = Product.objects.all()
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else: 
+        try:
+            if (request.user.is_superuser or
+                    request.user.swimmer.graduation_year is not None):
+                products = Product.objects.all()
+    
+        except Swimmer.DoesNotExist:
+            return HttpResponseForbidden()
+
     return render(request, 'products/products.html', {'products': products})
 
 
 def product_detail(request, pk):
-    products = Product.objects.all()
-    product = get_object_or_404(Product, pk=pk)
+    if not request.user.is_authenticated:
+        return redirect('login')
+    else: 
+        try:
+            if (request.user.is_superuser or
+                    request.user.swimmer.graduation_year is not None):
+                products = Product.objects.all()
+                product = get_object_or_404(Product, pk=pk)
+    
+        except Swimmer.DoesNotExist:
+            return HttpResponseForbidden()
+
     return render(request, 'products/productdetail.html', {'product': product,
                   'products': products})
