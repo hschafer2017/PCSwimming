16574c0cde887ed3109a8ff62440e47a92437a98
Haley Schafer <hschafer@umich.edu>
2018-08-26 11:10:54 +0000

    HTML cleanup for home and file structure edits for static

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
-# Create your views here.
-def get_index(request): 
-    if request.user.is_authenticated: 
+
+def get_index(request):
+    if request.user.is_authenticated:
         return redirect('get_events')
-    return render(request, 'home/landing.html')
+    return render(request, 'home/index.html')
